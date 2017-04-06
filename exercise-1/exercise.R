ab3195e465b82fb098562e81cf9c19e7105bfbda
Christy Lu <hyl9@uw.edu>
2017-04-06 13:58:34 -0700

    edited

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,27 +1,27 @@
 # Exercise 1: Creating and Indexing Vectors
-
+v <- c(1, 2, 3)
 # Create a vector `first.ten` that has the values 10 through 20 in it (using the : operator)
-
+first.ten <- c(10 : 20)
 
 # Create a vector `next.ten` that has the values 21 through 30 in it (using the seq operator)
-
+next.ten <- c(21 : 30)
 
 # Create a vector `all.numbers` by combining the vectors `first.ten` and `next.ten`
-
+all.numbers <- first.ten + next.ten
 
 # Create a variable `eleventh` that is equal to the 11th element in your vector `all.numbers`
-
+eleventh <- all.numbers[11]
 
 # Create a vector `some.numbers` that is equal to the second through the 5th elements of `all.numbers`
-
+some.numbers <- c(all.numbers[2 : 5])
 
 # Create a variable `len` that is equal to the length of your vector `all.numbers`
-
+len <- length(all.numbers)
 
 ### Bonus ###
 
 # Create a vector `odd` that holds the odd numbers from 1 to 100
-
+odd <- c()
 
 # Using the `all` and `%%` operators, confirm that all of the numbers in your `odd` vector are odd
 
--- exercise-3/exercise.R
+++ exercise-3/exercise.R
@@ -1,18 +1,27 @@
 # Exercise 3: Vector and function practice
 
 # Create a vector `marbles` with 6 different colors in it (representing marbles)
-
+marbles <- c('red', 'orange', 'yellow', 'green', 'blue', 'purple')
 
 # Use the `sample` function to select a single marble
-
+sample(marbles, size = 1)
 
 # Write a function MarbleGame that does the following:
 # - Takes in a `guess` of a marble color
 # - Randomly samples a marble
 # - Returns whether or not the person guessed accurately (preferrably a full phrase)
-
+MarbleGame <- function(guess) {
+  ans <- sample(marbles, 1)
+  if(ans == guess) {
+    return ("boooooo")
+  } else {
+    return ("you're correct!")
+  }
+  
+}
 
 # Play the marble game!
+MarbleGame("red")
 
 
 # Bonus: Play the marble game until you win, keeping track of how many tries you take
@@ -20,3 +29,4 @@
 
 ## Double bonus(answer not provided): play the game 1000X (until you win) and track the average number of tries
 # Is it what you expected based on the probability
+
